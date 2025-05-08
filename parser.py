44d1004fb61a9ccae542c7eaeef239b8ac04ea90
Danila Slesarev <blackdeer1524@yandex.ru>
2025-05-08 22:57:49 +0300

    checkpoint

--- parser.py
+++ parser.py
@@ -41,21 +41,54 @@ class Text:
         self._cur = None
 
 
+class TextWithPosition(Text):
+    def __init__(self, f: TextIO):
+        super().__init__(f)
+        self._row = 1
+        self._col = 1
+
+    def position(self) -> Position:
+        return Position(self._row, self._col)
+
+    def peek(self):
+        r = super().peek()
+        if r == "\r":
+            return "\n"
+        return r
+
+    def advance(self):
+        cur = self.peek()
+        if cur is None:
+            return
+
+        if cur == "\r":
+            super().advance()
+            next = self.peek()
+            if next == "\n":
+                super().advance()
+            self._row += 1
+            self._col = 1
+        elif cur == "\n":
+            super().advance()
+            self._row += 1
+            self._col = 1
+        else:
+            self._col += 1
+            super().advance()
+
+
 @dataclass(frozen=True)
 class Segment:
     start: Position
     end: Position
 
 
-# ================= Terminals =================
+# ==============================================
 
 
 @dataclass(frozen=True)
-class LeftParen(Segment): ...
-
-
-@dataclass(frozen=True)
-class RightParen(Segment): ...
+class QuotedString(Segment):
+    value: str
 
 
 @dataclass(frozen=True)
@@ -82,6 +115,10 @@ class Or(Segment): ...
 class End(Segment): ...
 
 
+@dataclass(frozen=True)
+class Epsilon(Segment): ...
+
+
 # ============================================
 
 
@@ -95,46 +132,20 @@ class ScanError:
     pos: Position
 
 
-Keyword = Axiom | End | Is | Or
+Keyword = Axiom | End | Is | Or | Epsilon
 
-Token = LeftParen | RightParen | Identifier | Keyword | EOF
+Token = QuotedString | Identifier | Keyword | EOF
 
 
 class Scanner:
     def __init__(self, f: TextIO):
-        self._text = Text(f)
-
-        self._row = 1
-        self._col = 1
-
-    def position(self) -> Position:
-        return Position(self._row, self._col)
+        self._text = TextWithPosition(f)
 
     def skip_spaces(self):
         while True:
             cur = self._text.peek()
             if cur is None or not cur.isspace():
                 return
-            self._advance()
-
-    def _advance(self):
-        cur = self._text.peek()
-        if cur is None:
-            return
-
-        if cur == "\r":
-            self._text.advance()
-            next = self._text.peek()
-            if next == "\n":
-                self._text.advance()
-            self._row += 1
-            self._col = 1
-        elif cur == "\n":
-            self._text.advance()
-            self._row += 1
-            self._col = 1
-        else:
-            self._col += 1
             self._text.advance()
 
     def parse_keyword(self):
@@ -147,7 +158,7 @@ class Scanner:
             cur = self._text.peek()
             if cur is None or cur.isspace():
                 break
-            self._advance()
+            self._text.advance()
 
     def assert_string(self, target: str):
         for i in range(len(target)):
@@ -169,153 +180,126 @@ class Scanner:
                 yield EOF()
                 return
             elif cur == "`":
-                errored = False  # В случае ошибки мы всё равно восстановимся
-                start_p = self.position()
-                self._advance()
+                errored = False  # NOTE: В случае ошибки мы всё равно восстановимся
+                start_p = self._text.position()
+                self._text.advance()
                 cur = self._text.peek()
                 if cur == "a":
                     if not self.assert_string("axiom"):
-                        err = ScanError("expected `axiom keyword", self.position())
+                        err = ScanError(
+                            "expected `axiom keyword", self._text.position()
+                        )
                         self.find_whitespace()
                         yield err
-                    end_p = self.position()
+                    end_p = self._text.position()
                     yield Axiom(start_p, end_p)
                 elif cur == "e":
                     if not self.assert_string("end"):
-                        err = ScanError("expected `end keyword", self.position())
+                        err = ScanError("expected `end keyword", self._text.position())
                         self.find_whitespace()
                         yield err
-                    end_p = self.position()
+                    end_p = self._text.position()
                     yield End(start_p, end_p)
                 elif cur == "i":
                     if not self.assert_string("is"):
-                        err = ScanError("expected `is keyword", self.position())
+                        err = ScanError("expected `is keyword", self._text.position())
                         self.find_whitespace()
                         yield err
-                    end_p = self.position()
+                    end_p = self._text.position()
                     yield Is(start_p, end_p)
                 elif cur == "o":
                     if not self.assert_string("or"):
-                        err = ScanError("expected `or keyword", self.position())
+                        err = ScanError("expected `or keyword", self._text.position())
                         self.find_whitespace()
                         yield err
-                    end_p = self.position()
+                    end_p = self._text.position()
                     yield Or(start_p, end_p)
+                elif cur == "e":
+                    if not self.assert_string("epsilon"):
+                        err = ScanError(
+                            "expected `epsilon keyword", self._text.position()
+                        )
+                        self.find_whitespace()
+                        yield err
+                    end_p = self._text.position()
+                    yield Epsilon(start_p, end_p)
                 else:
-                    err = ScanError("expected `or keyword", self.position())
+                    err = ScanError("expected a keyword", self._text.position())
                     self.find_whitespace()
                     yield err
-            elif cur == "(":
-                start_p = self.position()
-                self._advance()
-                end_p = self.position()
-                yield LeftParen(start_p, end_p)
-                errored = False
-                continue
-            elif cur == ")":
-                start_p = self.position()
-                self._advance()
-                end_p = self.position()
-                yield RightParen(start_p, end_p)
-                errored = False
-                continue
             elif cur == '"':
-                start_p = self.position()
-                self._advance()
+                start_p = self._text.position()
+                value = ""
+                self._text.advance()
                 while True:
                     cur = self._text.peek()
                     if cur is None or cur in ('"', "\n"):
                         break
+                    value += cur
                 if cur is None or cur == "\n":
-                    yield ScanError('expected a closing quote (")', self.position())
+                    yield ScanError(
+                        'expected a closing quote (")', self._text.position()
+                    )
                     continue
-                end_p = self.position()
-                self._advance()
-                yield Identifier(start_p, end_p, )
+                end_p = self._text.position()
+                self._text.advance()
+                yield QuotedString(start_p, end_p, value)
                 errored = False
                 continue
+            elif cur.isalpha():
+                start_p = self._text.position()
+                value = cur
+                self._text.advance()
+                while True:
+                    cur = self._text.peek()
+                    if cur is None or not cur.isalnum() or cur not in ("_", "'"):
+                        break
+                    value += cur
+                end_p = self._text.position()
+                yield Identifier(start_p, end_p, value)
             else:
                 if errored:
-                    self._advance()
+                    self._text.advance()
                     continue
                 errored = True
-                yield ScanError(f"unexpected symbol: {cur}", self.position())
-                self._advance()
+                yield ScanError(f"unexpected symbol: {cur}", self._text.position())
+                self._text.advance()
 
 
-NON_TERMINALS = Literal["E", "E1", "T", "T1", "F"]
-TERMINALS = Literal["n", "(", "+", "*", ")", "$"]
-EPSILON_T = Literal["EPSILON"]
-EPSILON: EPSILON_T = "EPSILON"
-
-
-@dataclass
-class Node:
-    name: EPSILON_T | TERMINALS | NON_TERMINALS
-    index: int
-    children: list["Node"]
-
-    def full_name(self) -> str:
-        return f'"{self.name} [{self.index}]"'
+NON_TERMINALS = Literal["S'", "S", "Rule", "RuleTail", "RuleAlt"]
+# TERMINALS = Type[QuotedString] | Type[Identifier] | Axiom | End | Is
+# EPSILON_T = Literal["EPSILON"]
+# EPSILON: EPSILON_T = "EPSILON"
 
 
 class Analyzer:
-    _table: dict[
-        NON_TERMINALS,
-        dict[
-            type[Identifier]
-            | type[LeftParen]
-            | type[Plus]
-            | type[Mult]
-            | type[RightParen]
-            | type[EOF],
-            list[TERMINALS | NON_TERMINALS | EPSILON_T],
-        ],
-    ] = {
-        "E": {
-            Identifier: ["T", "E1"],
-            LeftParen: ["T", "E1"],
-            Plus: [],
-            Mult: [],
-            RightParen: [],
-            EOF: [],
-        },
-        "E1": {
-            Identifier: [],
-            LeftParen: [],
-            Plus: ["+", "T", "E1"],
-            Mult: [],
-            RightParen: ["EPSILON"],
-            EOF: ["EPSILON"],
-        },
-        "T": {
-            Identifier: ["F", "T1"],
-            LeftParen: ["F", "T1"],
-            Plus: [],
-            Mult: [],
-            RightParen: [],
-            EOF: [],
-        },
-        "T1": {
-            Identifier: [],
-            LeftParen: [],
-            Plus: ["EPSILON"],
-            Mult: ["*", "F", "T1"],
-            RightParen: ["EPSILON"],
-            EOF: ["EPSILON"],
-        },
-        "F": {
-            Identifier: ["n"],
-            LeftParen: ["(", "E", ")"],
-            Plus: [],
-            Mult: [],
-            RightParen: [],
-            EOF: [],
-        },
-    }
-
     def __init__(self, s: Scanner):
         self.scanner = s
+
+    def transitions(self, t: Token):
+        match t:
+            case QuotedString():
+                return {
+                    "S'": [],
+                    "S": [],
+                    "Rule": [],
+                    "RuleTail": [],
+                    "RuleAlt": [],
+                }
+            case Identifier():
+                ...
+            case Axiom():
+                ...
+            case End():
+                ...
+            case Is():
+                ...
+            case Or():
+                ...
+            case Epsilon():
+                ...
+            case EOF():
+                ...
 
     def parse(self):
         depth_siblings: DefaultDict[int, list[str]] = defaultdict(lambda: [])
@@ -336,7 +320,7 @@ class Analyzer:
                 depth_siblings[depth].append(cur_node.full_name())
                 match cur_node.name:
                     case "n":
-                        if type(token) != Identifier:
+                        if type(token) != QuotedString:
                             raise RuntimeError(
                                 f"expected Identifier, but {type(token)} found"
                             )
@@ -379,7 +363,7 @@ class Analyzer:
                                 | Mult()
                                 | LeftParen()
                                 | RightParen()
-                                | Identifier()
+                                | QuotedString()
                                 | EOF()
                             ):
                                 rule = expected[type(token)]
