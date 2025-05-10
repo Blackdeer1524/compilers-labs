66fd27dd97e3cd2738c4adb16bfea3580a4f88c9
Danila Slesarev <blackdeer1524@yandex.ru>
2025-05-10 16:12:37 +0300

    rudimentary SemanticsAnalyzer

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from pprint import pprint
 from src.analysis.analyzer import Analyzer
 from src.scanning.scanner import Scanner
+from src.table_synthesis.synthesizer import SemanticsAnalyzer
 
 
 def main():
     s = Scanner(open("./test.txt"))
     a = Analyzer(s)
     res = a.parse()
-    print("digraph {")
-    print(res.to_graphviz().replace("\r", "\\n"))
-    print("}")
+    # print("digraph {")
+    # print(res.to_graphviz().replace("\r", "\\n"))
+    # print("}")
+
+    sa = SemanticsAnalyzer()
+    pprint(sa.process_productions(res))
 
 
 if __name__ == "__main__":
--- src/table_synthesis/synthesizer.py
+++ src/table_synthesis/synthesizer.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 from typing import Literal, Optional
-from scanning.scanner import Ident, QuotedStr
+from src.scanning.scanner import Ident, QuotedStr
 from src.analysis.ast import *
-from text.processors import Position
+from src.text.processors import Position
 
 RULE_T = list[Ident | QuotedStr | Literal["epsilon"]]
+AXIOM_INTO_T = tuple[str | None, Position | None] | None
 
 
 @dataclass
 class ProductionInfo:
-    lhs: Ident | None
+    lhs: Ident
     rhs: list[RULE_T]
     is_axiom: bool
 
@@ -21,10 +22,11 @@ class SemanticsAnalyzer:
     def store_error(self, mes: str, pos: Position | None):
         self.errors.append(f"[{pos if pos is not None else '???'}] {mes}")
 
-    def is_axiom(self, node: AxiomNode) -> bool:
+    def collect_axiom(self, node: AxiomNode) -> bool:
         return node.value is not None
 
     def collect_nonterm(self, node: NonTermNode) -> Optional[Ident]:
+
         if node.value is None:
             self.store_error(f"expected {node.node_label} not to be None", node.pos)
             return None
@@ -74,33 +76,80 @@ class SemanticsAnalyzer:
             case None:
                 return []
             case (
-                KeywordNode() as kw_is,
+                KeywordNode() as kw_or,
                 RuleNode() as rule,
                 RuleAltNode() as alt_rules,
             ):
-                if kw_is.kind != "is":
+                if kw_or.kind != "or":
                     self.store_error(
-                        f'expected "is" keyword, but {kw_is.kind} found', kw_is.pos
+                        f'expected "or" keyword, but "{kw_or.kind}" found', kw_or.pos
                     )
-                if kw_is.value is None:
+                if kw_or.value is None:
                     self.store_error(
-                        f"expected {kw_is.node_label} not to be None", kw_is.pos
+                        f"expected {kw_or.node_label} not to be None", kw_or.pos
                     )
                 return [self.collect_rule(rule)] + self.collect_alt_rules(alt_rules)
 
-    def process_node(self, node: ProductionNode) -> list[ProductionInfo]:
+    def collect_init_node(self, node: InitNode) -> ProductionNode | None:
         if node.value is None:
-            return []
+            self.store_error(f"expected {node.node_label} not to be None", node.pos)
+            return None
+        return node.value[0]
 
-        (axiom, nonterm, kw_is, rhs, alt_rules, kw_end, next_prod) = node.value
+    def process_productions(
+        self, node: InitNode
+    ) -> tuple[AXIOM_INTO_T, dict[str, ProductionInfo]] | list[str]:
+        cur_opt = self.collect_init_node(node)
+        if cur_opt is None:
+            errs = self.errors
+            self.errors = []
+            return errs
+        cur = cur_opt
 
-        is_axiom = self.is_axiom(axiom)
-        lhs = self.collect_nonterm(nonterm)
-        if kw_is.value is None:
-            self.store_error(f"expected {kw_is.node_label} not to be None", kw_is.pos)
-        rhs = [self.collect_rule(rhs)] + self.collect_alt_rules(alt_rules)
-        if kw_end.value is None:
-            self.store_error(f"expected {kw_end.node_label} not to be None", kw_end.pos)
+        axiom_info: AXIOM_INTO_T = None
+        res: dict[str, ProductionInfo] = {}
+        while True:
+            if cur.value is None:
+                break
 
-        res = ProductionInfo(lhs, rhs, is_axiom)
-        return [res] + self.process_node(next_prod)
+            (kw_axiom, nonterm, kw_is, rhs, alt_rules, kw_end, next_prod) = cur.value
+
+            is_axiom = self.collect_axiom(kw_axiom)
+            lhs = self.collect_nonterm(nonterm)
+            if kw_is.value is None:
+                self.store_error(
+                    f"expected {kw_is.node_label} not to be None", kw_is.pos
+                )
+
+            rhs = [self.collect_rule(rhs)] + self.collect_alt_rules(alt_rules)
+
+            if kw_end.value is None:
+                self.store_error(
+                    f"expected {kw_end.node_label} not to be None", kw_end.pos
+                )
+
+            if is_axiom:
+                if axiom_info is not None:
+                    self.store_error(
+                        f"found another axiom. prev definition: {axiom_info}",
+                        kw_axiom.pos,
+                    )
+                else:
+                    axiom_info = (lhs.value if lhs is not None else None, kw_axiom.pos)
+
+            if lhs is not None:
+                if (prev_def := res.get(lhs.value)) is None:
+                    res[lhs.value] = ProductionInfo(lhs, rhs, is_axiom)
+                else:
+                    self.store_error(
+                        f"found redefinition of {lhs.value}. Prev definition: {prev_def.lhs.start}",
+                        lhs.start,
+                    )
+            cur = next_prod
+
+        if len(self.errors) > 0:
+            errs = self.errors
+            self.errors = []
+            return errs
+
+        return (axiom_info, res)
